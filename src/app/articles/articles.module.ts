1d54b64ef8ec877d059274bb1bd2d4d7b9706424
Cory Mortimer <cory.m.mortimer@gmail.com>
2018-07-24 20:42:08 -0700

    Add article component to display new news story
    Added an article component to display news story. This will be reused
    in the future to easily display other news stories.

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { CovalentMarkdownModule } from '@covalent/markdown';
 
 import { ArticlesComponent } from './articles.component';
 import { ArticlesService } from './articles.service';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    CovalentMarkdownModule,
     MatDividerModule,
     MatListModule
   ],
-  declarations: [ArticlesComponent],
+  declarations: [ArticlesComponent, ArticleComponent],
   providers: [ArticlesService],
-  exports: [ArticlesComponent]
+  exports: [ArticlesComponent, ArticleComponent]
 })
 export class ArticlesModule { }
