22ca10d100d8ed0e64b52190e549dcaa75609b22
Cory Mortimer <cory.m.mortimer@gmail.com>
2018-10-18 21:31:19 -0700

    Turn news page into a card layout
    The news page was just a list on a page. Add the card layout to make it
    more appealing. Also, add a link to the news creator page from the news
    page.

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+import { appear } from '../animations';
+
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+  styleUrls: ['./news.component.scss'],
+  animations: appear
 })
 export class NewsComponent { }
--- src/app/news/news.module.ts
+++ src/app/news/news.module.ts
@@ -4,6 +4,7 @@ import { CovalentLayoutModule } from '@covalent/core/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { NewsRoutingModule } from './news-routing.module';
 import { NewsComponent } from './news.component';
@@ -17,6 +18,7 @@ import { NewsArticleComponent } from './news-article/news-article.component';
     MatButtonModule,
     MatIconModule,
     MatCardModule,
+    MatDividerModule,
     ArticlesModule,
     NewsRoutingModule
   ],
